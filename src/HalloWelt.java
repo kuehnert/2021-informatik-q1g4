aa602cbfa92a3a60c1d8f73ec9bf79657e091457
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2021-09-09 12:46:34 +0200

    Initial Commit

---
+++ src/HalloWelt.java
@@ -0,0 +1,11 @@
+public class HalloWelt {
+    public static void main(String[] args) {
+        System.out.println("Hallo Welt 3!");
+    }
+}
+
+/*
+    Schreiben Sie eine neue Klasse "Zaehlen",
+    die beim Aufruf die Quadratzahlen (1, 4, 9, 16, ...)
+    bis 100 zusammenaddiert.
+ */
