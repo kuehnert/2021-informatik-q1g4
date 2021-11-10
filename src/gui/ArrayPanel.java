2ea9b80115d57d03095f7549774bd5d857321c3e
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2021-11-10 09:48:19 +0100

    Stunde vom 10.11.2021

--- src/gui/ArrayPanel.java
+++ src/gui/ArrayPanel.java
@@ -6,11 +6,21 @@ import java.awt.*;
 public class ArrayPanel extends JPanel {
     private static final String uiClassID = "ArrayPanel";
     private int[] array;
+    private int maxValue;
 
     public ArrayPanel() {
         super(true);
         setPreferredSize(new Dimension(200, 200));
-        array = new int[]{3, 1, 4, 5, 2, 8, 10 , 6, 7, 9};
+        setArray(new int[]{3, 1, 12, 25, 20, 5, 48, 5, 4, 5, 2, 8, 10, 6, 7, 9});
+    }
+
+    public void setArray(int[] array) {
+        this.array = array;
+        maxValue = array[0];
+        for (int e : array) {
+            maxValue = Math.max(maxValue, e);
+        }
+        repaint();
     }
 
     @Override
@@ -18,16 +28,16 @@ public class ArrayPanel extends JPanel {
         super.paint(g);
         Rectangle r = g.getClipBounds();
 
-        int colWidth = r.width / array.length;
+        double colWidth = r.width / (double) array.length;
         g.setColor(Color.BLUE);
 
         for (int i = 0; i < array.length; i++) {
-            int hoehe = array[i] * r.height / array.length;
-            g.fillRect(r.x + i * colWidth, r.y+r.height-hoehe, colWidth, hoehe);
+            int hoehe = array[i] * r.height / maxValue;
+            g.fillRect(r.x + (int) Math.round(i * colWidth), r.y + r.height - hoehe, (int) Math.round(colWidth), hoehe);
         }
 
         g.setColor(Color.BLACK);
-        g.drawRect(r.x, r.y, r.width-1, r.height-1);
+        g.drawRect(r.x, r.y, r.width - 1, r.height - 1);
     }
 }
 // r.x = 0; r.y = 0; r.width = 200; r.height = 200; colWidth = 20; a.length = 10
