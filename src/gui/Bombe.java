aebe91cfeb8c8a218e4801196bf44fb1ff3583a0
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2021-11-29 13:55:24 +0100

    Stunde vom 29.11.2021

--- src/gui/Bombe.java
+++ src/gui/Bombe.java
@@ -2,24 +2,34 @@ package gui;
 
 import util.PlaySound;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 public class Bombe extends JFrame {
     private JButton bClick;
     private JPanel pMain;
     private int counter;
+    private ImageIcon iBombe2;
+    private Image background;
 
     public Bombe() {
         super();
         counter = 5;
+        try {
+            iBombe2 = new ImageIcon(ImageIO.read(getClass().getResource("../bombe2.png")));
+            background = Toolkit.getDefaultToolkit().createImage("background.jpg");
+        } catch (IOException e) {}
 
         bClick.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (counter > 1) {
                     bClick.setText("Klick mich... " + --counter);
+                    bClick.setIcon(iBombe2);
                 } else {
                     bClick.setText("Bumm!");
                     PlaySound.playSound("bombe.wav");
@@ -33,6 +43,11 @@ public class Bombe extends JFrame {
         setSize(800, 600);
         setVisible(true);
     }
+
+    // void paint(Graphics g) {
+    //     super(g);
+    //     g.drawImage(background, 0, 0, null);
+    // }
 
     public static void main(String[] args) {
         new Bombe();
