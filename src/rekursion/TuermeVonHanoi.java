f761bd12d5f8a66b8faea31929c9ada86de34e2e
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2022-01-26 10:19:47 +0100

    Stunde vom 26.01.2022

--- src/rekursion/TuermeVonHanoi.java
+++ src/rekursion/TuermeVonHanoi.java
@@ -1,20 +1,26 @@
 package rekursion;
 
 public class TuermeVonHanoi {
-    public static void tuerme(int anzahl, String start, String ziel, String zwischen) {
+    static int bewegungenLuca = 2;
+    static int bewegungenRekursiv = 0;
+
+    private static void _tuerme(int anzahl, String start, String ziel, String zwischen) {
         if (anzahl > 1) {
-            tuerme(anzahl-1, start, zwischen, ziel);
+            _tuerme(anzahl - 1, start, zwischen, ziel);
         }
 
         System.out.println("Verschiebe Scheibe von " + start + " nach " + ziel);
 
         if (anzahl > 1) {
-            tuerme(anzahl-1, zwischen, ziel, start);
+            _tuerme(anzahl - 1, zwischen, ziel, start);
         }
     }
 
-    static int bewegungenLuca = 2;
-    public static void bewegungenLuca(int anzahl) {
+    public static void tuerme(int anzahl) {
+        _tuerme(anzahl, "1", "3", "2");
+    }
+
+        public static void bewegungenLuca(int anzahl) {
         if (anzahl > 1) {
             bewegungenLuca = bewegungenLuca * 2;
             bewegungenLuca(anzahl - 1);
@@ -37,30 +43,62 @@ public class TuermeVonHanoi {
         return bewegungen;
     }
 
-    static int bewegungenRekursiv = 0;
-    public static void bewegungenRekursiv(int anzahl) {
+    private static void _bewegungenRekursiv(int anzahl) {
         if (anzahl > 1) {
-            bewegungenRekursiv(anzahl-1);
+            _bewegungenRekursiv(anzahl - 1);
         }
 
         bewegungenRekursiv = bewegungenRekursiv + 1;
 
         if (anzahl > 1) {
-            bewegungenRekursiv(anzahl-1);
+            _bewegungenRekursiv(anzahl - 1);
         }
     }
 
-    public static void main(String[] args) {
-        // tuerme(10, "Turm 1", "Turm 3", "Turm 2");
-        int scheiben = 4;
-        bewegungenLuca(scheiben);
-        System.out.printf("Für %d Scheiben braucht man %d Züge%n", scheiben, bewegungenLuca);
-        System.out.printf("Für %d Scheiben braucht man %d Züge%n", scheiben, bewegungenFormel(scheiben));
-        System.out.printf("Für %d Scheiben braucht man %d Züge%n", scheiben, bewegungenIterativ(scheiben));
+    // Starter-Methode
+    public static void bewegungenRekursiv(int anzahl) {
+        bewegungenRekursiv = 0;
+        _bewegungenRekursiv(anzahl);
+    }
 
-        System.out.println(bewegungenRekursiv);
-        bewegungenRekursiv(scheiben);
-        System.out.println(bewegungenRekursiv);
+    public static int bewegungenRekursivOhneNebeneffekte(int anzahl) {
+        int bewegungen = 0;
+
+        if (anzahl > 1) {
+            bewegungen = bewegungen + bewegungenRekursivOhneNebeneffekte(anzahl - 1);
+        }
+
+        bewegungen = bewegungen + 1;
+
+        if (anzahl > 1) {
+            bewegungen = bewegungen + bewegungenRekursivOhneNebeneffekte(anzahl - 1);
+        }
+
+        return bewegungen;
+    }
+
+    public static int bewegungenRekursivOhneNebeneffekte2(int anzahl) {
+        int bewegungen = 1;
+
+        if (anzahl > 1) {
+            bewegungen = bewegungen + 2 * bewegungenRekursivOhneNebeneffekte2(anzahl - 1);
+        }
+
+        return bewegungen;
+    }
+
+    public static void main(String[] args) {
+        // tuerme(10);
+
+        for (int scheiben = 1; scheiben < 7; scheiben++) {
+            // bewegungenLuca(scheiben);
+            // System.out.printf("Für %d Scheiben braucht man %d Züge%n", scheiben, bewegungenLuca);
+            // System.out.printf("Für %d Scheiben braucht man %d Züge%n", scheiben, bewegungenFormel(scheiben));
+            // System.out.printf("Für %d Scheiben braucht man %d Züge%n", scheiben, bewegungenIterativ(scheiben));
+            System.out.printf("Für %d Scheiben braucht man %d Züge%n", scheiben, bewegungenRekursivOhneNebeneffekte(scheiben));
+            System.out.printf("Für %d Scheiben braucht man %d Züge%n", scheiben,
+                    bewegungenRekursivOhneNebeneffekte2(scheiben));
+        }
     }
 }
 
