b57c63bea8d4e7e6f2715cd8648d957999e585c5
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2022-02-09 10:22:42 +0100

    Stunde vom 09.02.2022

---
+++ src/rekursion/Fibonacci.java
@@ -0,0 +1,39 @@
+package rekursion;
+
+public class Fibonacci {
+    // Memoization
+    static long[] memos = new long[1000];
+
+    public static long fibonacciRekursiv(int n) {
+        System.out.println("Berechne f(" + n + ")");
+
+        // Trivialfall, nicht-rekursiver Fall
+        if (n <= 1) {
+            return 1;
+        } else {
+            // Rekursiver Fall
+            return fibonacciRekursiv(n - 1) + fibonacciRekursiv(n - 2);
+        }
+    }
+
+    public static long fibonacciRekursivMemo(int n) {
+        if (n <= 1) {
+            return 1;
+        } else {
+            return fibonacciRekursivMemo(n - 1) + fibonacciRekursivMemo(n - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        long start, result, duration;
+        fibonacciRekursiv(20);
+        System.out.println(counter + " Aufrufe");
+
+        // for (int i = 30; i <= 52; i++) {
+        //     start = System.currentTimeMillis();
+        //     result = fibonacciRekursiv(i);
+        //     duration = System.currentTimeMillis() - start;
+        //     System.out.printf("f(%2d) = %15d; Dauer %.2f%n", i, result, duration / 1000.0);
+        // }
+    }
+}
